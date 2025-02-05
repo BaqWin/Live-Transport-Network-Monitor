590042ed294c641227acbc4eb9bd0b87e243dc7c
Marcin <marcinjagielak90@gmail.com>
2025-02-05 23:22:41 +0100

    Add Boost as dependency, clean build script

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,16 @@
 from conan import ConanFile
 
-class ConanPackage(ConanFile):
-    name = 'network-monitor'
+class NetworkMonitorConan(ConanFile):
+    name = "network-monitor"
     version = "0.1.0"
     settings = "os", "arch", "compiler", "build_type"
 
     generators = "CMakeDeps", "CMakeToolchain"
 
     requires = [
-
+        "boost/1.82.0",
     ]
+
+    default_options = {
+        "boost/*:shared": False,
+    }
